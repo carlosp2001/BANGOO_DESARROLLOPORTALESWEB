8c5bc2712e9331eabfc90129bdcd445b6626a99f
carlosp2001 <85173449+carlosp2001@users.noreply.github.com>
2021-12-02 08:45:05 -0600

    Agregada subida de imagen guardada en objeto para lectura

--- js/moduloproductos.js
+++ js/moduloproductos.js
@@ -1,18 +1,41 @@
-
+function Producto(nombre, cantidad, tipo, talla, color, categoria, img){
+    this.nombre = nombre
+    this.cantidad =  cantidad
+    this.tipo = tipo
+    this.talla = talla
+    this.color = color
+    this.categoria = categoria
+    this.img = img
+}
 
 function guardar_localstorage(){
+    var reader = new FileReader();
+    var img = document.getElementById("customFile").files[0];
     var input_nombre = document.getElementById("Nombre_Producto");
     var input_cantidad = document.getElementById("Cantidad_producto");
-    var input_tipo = document.getElementById("Tipo_Producto_Dropdown");
+    var input_tipo = document.getElementById("tipopr_select");
+    var input_color = document.getElementById("color_picker");
     var input_talla = document.getElementById("Talla_numero");
-    console.log("txt");
+    var input_categoria = document.getElementById("categoria-select");
 
-    let producto = {
-        nombre: input_nombre.value,
-        cantidad:input_cantidad.value,
-        tipo:input_tipo.value,
-        talla:input_talla.value
-
-    }
-    localStorage.setItem("producto", JSON.stringify(producto));
+    reader.addEventListener('load', function (){
+        if(this.result){
+            producto = new Producto(input_nombre.value, input_cantidad.value, input_tipo.value, input_color.value,
+                input_talla.value, input_categoria.value, img = this.result)
+            localStorage.setItem(((localStorage.length+1).toString()), JSON.stringify(producto));
+        }
+    })
+    reader.readAsDataURL(document.getElementById("customFile").files[0])
 }
+
+function agregar_imagen(){
+    var imglocal = JSON.parse(localStorage.getItem("1"));
+    console.log(imglocal);
+    var divresult = document.getElementById("result")
+
+    var img = document.createElement("img");
+    img.src= imglocal.img;
+    divresult.appendChild(img)
+
+}
+
